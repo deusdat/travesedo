a1300f71b8502a182dbd4daab45e35c5a3c4e120
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2014-08-25 17:03:35 -0400

    Fixed the client. Got rid of the crazy API

--- src/travesedo/http.clj
+++ src/travesedo/http.clj
@@ -1,5 +1,6 @@
 (ns travesedo.http
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.string :as string]))
 
 (def handler-lookup {:get client/get 
                      :post client/post 
@@ -7,26 +8,36 @@
                      :put client/put
                      :delet client/delete})
 (def core-params {:as :json 
+                  :content-type :json
                   :coerce :always
                   :throw-exceptions false})
 
 (def exceptional #{400})
 
-(defn with-query-parm 
-  "Adds a query parameter to the request configuration"
-  [p-key p-value req]
-  )
-;; FIXME need to support config object rather than a never ending line of args.
-(defn send-request 
-  ([action url]
-   (send-request action url nil nil))
-  ([action url query-params]
-   (send-request action url query-params nil))
-  ([action url query-params body]
-   (let [handler (action handler-lookup)
-         data-params (if (map? body) {:form-params body} {:body body})
-         query-params {:query-params query-params}
-         req-details (merge query-params data-params core-params)
-         {res-body :body} (handler url req-details)]
-     ;; Put logic here to handle real errors
-     res-body)))
+(defn execute 
+  "Calls a specified coordinator with the desired request spec. :conns should be a list of connection configurations
+  for the present cluster. If the ArangoDB instance only has one conns can be a single map with expected keys."
+  [{conns :conns
+    resource :resource 
+    method :method 
+    query-params :query-params 
+    header-params :header-params
+    conn-picker :conn-picker
+    body :body
+    :as req }]
+  {:pre [(not (nil? resource))]}
+  (let [handler (method handler-lookup)
+        conn ((or conn-picker #(rand-nth %)) (if (vector? conns) conns [conns]))
+        url (build-url conn resource)
+        data-params (if (map? body) {:form-params body} {:body body})
+        req-details (merge core-params data-params query-params)
+        {res-body :body status :status :as res} (handler url req-details body)]
+    (println handler)
+    (println url)
+    (println req-details)
+    res-body
+    ))
+
+(defn build-url [{db-url :db-url} resource]
+  (let [cleaned-url (string/replace db-url #"(?<=.)/$" "")]
+    (str cleaned-url resource)))
--- src/travesedo/util.clj
+++
@@ -1,4 +0,0 @@
-(ns travesedo.util)
-
-(defn add-config [config new-key value]
-  (assoc config new-key value))
