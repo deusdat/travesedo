641f4b909657e4b92c9421f6d0c8f8d67df19d0b
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2014-08-28 18:45:40 -0400

    Added collection api feature.

--- src/travesedo/collection.clj
+++ src/travesedo/collection.clj
@@ -1,32 +1,46 @@
 (ns travesedo.collection
-  (:require [travesedo.common]))
+  (:require [travesedo.common :refer :all]))
 
 (defn get-collection-root
   [{db-name :db-name}]
-    (str "/_db" db-name "/_api/collection/"))
+    (str "/_db/" db-name "/_api/collection/"))
 
-(defn get-resource 
-  [{db-name :db-name coll :collection :as config}]
-  (let [coll-name (if (map? coll) (:name coll) coll)]
-    (get-collection-root coll-name)))
+(defn get-collection-name 
+  [{coll :collection}]
+  (if (map? coll) (:name coll) coll))
 
+(defn get-named-resource 
+  [{db-name :db-name coll :collection :as config} & extra]
+  (let [coll-name (get-collection-name config)]
+    (apply str (get-collection-root config) coll-name extra)))
 
+(defn create-body
+  [{collection :collection :as config}]
+  (if (map? collection) collection {:name collection}))
 
-(create-collection!
+(defn create-collection!
   [config]
     (let [body (create-body config)
           resource (get-collection-root config)]
-      (with-req config :resource resource  :method :post :body body)))
+      (with-req config :resource resource :method :post :body body)))
 
-(delete-collection!
+(defn delete-collection!
   [config]
-  )
+  (with-req config :resource (get-named-resource config) :method :delete))
 
-(collection-stats
+(defn truncate-collection
+  [config]
+  (with-req config :resource (get-named-resource config "/truncate") :method :put))
+
+(defn collection-stats
   "Returns a list of collection statics like document count."
   [config]
-  )
+  (with-req config :resource (get-named-resource config "/figures") :method :get))
 
-(list-collections
+(defn collection-properties
   [config]
-  )
+    (with-req config :resource (get-named-resource config "/properties") :method :get))
+
+(defn list-collections
+  [config]
+  (with-req config :resource (get-named-resource config) :method :get))
--- src/travesedo/common.clj
+++ src/travesedo/common.clj
@@ -1,5 +1,6 @@
 (ns travesedo.common
-  (:require [travesedo.http :as client]))
+  (:require [travesedo.http :as client]
+            [clojure.string :as cstr]))
 
 (def exceptional-codes #{403 400 409})
 
@@ -25,18 +26,39 @@
 
 (def not-nil? (complement nil?))
 
+(defn camelize [input-string] 
+  (let [words (cstr/split input-string #"[\s_-]+")] 
+    (cstr/join "" (cons (cstr/lower-case (first words)) (map cstr/capitalize (rest words))))))
+
+
+(defn add-query-params
+  "Moves driver configuration values into their query parameter locations for the request.
+  It will merge any values in :query-params preset by the driver.
+
+  Returns a new configuration with the original values and the :request-params."
+  [config]
+  (create-params config :query-params nil :wait-for-sync :exclude-system :create-collection))
+
+;; FIXME to make key-mapper passed in better like :as key-mapper-fn or something.
+(defn create-params
+  ([config target-holder key-mapper & config-keys]
+   (let [params (select-keys config config-keys)
+         k-m (or key-mapper identity)
+         params (into {} (for [[k v] params] [(camelize (k-m (name k))) v]))]
+     (merge-with merge config {target-holder params}))))
+
 (defn keep-key-when-value
   [coll keep-value?]
   (select-keys coll (for [[k v] coll :when (keep-value? v)] k)))
 
+
 (defn move-headers [config]
-   (let [{async :async} config
-         ;; FIXME change the name on the key and value into a mapped over.
-         header-params (keep-key-when-value {"x-arango-async" (name (or async "false"))} not-nil? )]
-        (merge config {:header-params header-params})))
+  (let [{async :async} config
+        ;; FIXME change the name on the key and value into a mapped over.
+        header-params (keep-key-when-value {"x-arango-async" (name (or  async "false"))} not-nil? )]
+    (merge config {:header-params header-params})))
 
 (defn with-req
   [base-config & added-configs]
   (let [full-config (move-headers (merge base-config (apply hash-map added-configs)))]
     (process-response (client/execute full-config))))
-
