8292b97666eee5b73149186fa4e2980e68f75c32
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2014-10-18 22:49:56 -0400

    Updated project.clj with new description and github url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject travesedo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A clojure driver for the ArangoDB server."
+  :url "https://github.com/deusdat/travesedo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
