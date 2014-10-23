c8e73cf87f9f7982e09fa07a08074cc762e9c6ac
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2014-10-22 23:40:21 -0400

    added more tests.

--- test/travesedo/integration_test.clj
+++ test/travesedo/integration_test.clj
@@ -1,9 +1,9 @@
 (ns travesedo.integration-test
   (:require [clojure.test :refer :all]
-                [travesedo.database :as db]
-                [travesedo.collection :as col]
-                [travesedo.query :as q]
-                [travesedo.document :as doc]))
+            [travesedo.database :as db]
+            [travesedo.collection :as col]
+            [travesedo.query :as q]
+            [travesedo.document :as doc]))
 
 ;; This is really an integration test that runs heavily over the
 ;; query API, but leverages the collections/documents too.
@@ -12,9 +12,8 @@
 ;; http://blog.raphinou.com/2009/03/generate-random-string-in-clojure.html
 
 (def VALID-CHARS
-  (map char (concat (range 48 58) ; 0-9
-    (range 66 91) ; A-Z
-    (range 97 123)))) ; a-z
+  (map char (concat  (range 66 91) ; A-Z
+                    (range 97 123)))) ; a-z
 
 (defn random-char []
   (rand-nth VALID-CHARS))
@@ -24,8 +23,8 @@
 
 ;; Connection
 (def ctx {:conn {:type :simple, :url "http://localhost:8529"}
-               :db  (random-str 15), :wait-for-sync :true} ;; wait to avoid write misses.
-               )
+          :db  (random-str 15), :wait-for-sync :true} ;; wait to avoid write misses.
+  )
 
 (defn setup-fixure [f]
   "Setups the database for the tests"
@@ -39,37 +38,54 @@
                 :payload {:name "Amber Davenport" :age 31})  )
   (try (f)
     (finally
-    (db/drop ctx))))
+     (db/drop ctx))))
 
 
 (use-fixtures :once setup-fixure)
 
+(defn no-error [resp]
+  (is (false? (:error resp))))
+
+(defn contains-n-docs [resp n]
+  (is (= n (count (:result resp)))))
+
+(defn amber? [person]
+  (is (= "Amber Davenport" (:name person))))
+
+(defn patrick? [person]
+  (is (= "JPatrick Davenport" (:name person))))
+
+(defn no-more [resp]
+  (is (false? (:has-more resp))))
+
 (deftest aql-query-batch-sizes
   (testing "Against non-existant collection"
-   (let [aql  "FOR p IN nonexistant-collection RETURN p"
-         batched (q/aql-query (assoc ctx :payload {:query aql :batch-size 1}))]
-     (is (nil? (:has-more batched))) "There shouldn't be a data indicator."
-     (is (= 404 (:code batched)) "Server should say not found.")
-     (is (:error batched))) "Should map the error properly.")
+    (let [aql  "FOR p IN nonexistant-collection RETURN p"
+          batched (q/aql-query (assoc ctx :payload {:query aql :batch-size 1}))]
+      (is (nil? (:has-more batched))) "There shouldn't be a data indicator."
+      (is (= 404 (:code batched)) "Server should say not found.")
+      (is (:error batched))) "Should map the error properly.")
+
   (testing "Query with more results."
     (let [aql "FOR p IN people RETURN p"
           batched (q/aql-query (assoc ctx :payload {:query aql :batch-size 1}))]
-      (is (false? (:error batched)))
+      (no-error batched)
       (is (true? (:has-more batched)))
-      (is (= 1 (count (:result batched))))
+      (contains-n-docs batched 1)
       (testing "Delete a cursor"
         (let [ cursor (select-keys batched [:cursor-id])
-              deleted-cursor (q/delete (conj ctx cursor))]
+               deleted-cursor (q/delete (conj ctx cursor))]
           (is (nil? (:has-more deleted-cursor)))
-          (is (false? (:error deleted-cursor)))
+          (no-error deleted-cursor)
           (is (= 202 (:code deleted-cursor)))))))
+
   (testing "Query all documents AQL"
     (let [aql "FOR p IN people RETURN p"
-           payload { :payload {:query aql :batch-size 1}}
-           batched (q/aql-query-all (conj ctx payload))]
-      (is (= 2 (:count batched)))
-      (is (false? (:error batched)))
-      )))
+          payload { :payload {:query aql :batch-size 1}}
+          batched (q/aql-query-all (conj ctx payload))]
+      (contains-n-docs batched 2)
+      (no-error batched)
+      (no-more batched))))
 
 (deftest parse-aql-tests
   (testing "Parsing a valid AQL."
@@ -77,13 +93,31 @@
           resp (q/parse-aql-query (assoc ctx :payload {:query aql}))]
       (is (= 200 (:code resp)))
       )))
-
 (deftest query-by-example
   (testing "Tries to query for a doc based on name example."
     (let [example {:example {:name "JPatrick Davenport"} :collection "people"}
           new-ctx (assoc ctx :payload example)
           resp (q/by-example new-ctx)
           person (first (:result resp))]
+      (patrick? person)
+      (contains-n-docs resp 1)
+     )))
 
-      (is (= 1 (count (:result resp))))
-      (is (= "JPatrick Davenport" (:name person ))))))
+(deftest return-all-docs
+  (testing "Gets all of the documents for a collection defined in the collection"
+    (let [payload {:payload {:collection "people"}}
+          new-ctx (conj payload ctx)
+          resp (q/return-all new-ctx)
+          col (sort #(compare (:name %) (:name %2)) (:result resp))
+          p1 (first col)
+          p2 (second col)]
+      (no-error resp)
+      (contains-n-docs resp 2)
+      (amber? p1)
+      (patrick? p2)))
+
+  (testing "Limiting the return to 1 doc"
+    (let [payload {:payload {:collection "people" :limit 1}}
+          new-ctx (conj payload ctx)
+          resp (q/return-all new-ctx)]
+      (contains-n-docs resp 1))))
